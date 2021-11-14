85476895c3f7ec26a80412e5c9bbd05a4850e3c0
Sankhadeep Pujaru <sankhadeep.pujaru2008@gmail.com>
2021-11-14 09:14:51 +0530

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -3,7 +3,7 @@ package utilities;
 public interface Constants {
 
 	String URL = " ";				// public static final		
-	String USERNAME = " ";				// public static final	
-	String PASSWORD = " ";				// public static final	
-	String FILEPATH = " ";				// public static final	
+	String USERNAME = " ";				
+	String PASSWORD = " ";				
+	String FILEPATH = " ";				
 }
